417864ad371d411b7b029cd770e7d89317a5afff
Mark <rens2588@gmail.com>
2020-10-16 15:26:44 +0300

    home_work18_demo_models

---
+++ src/app/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls, name='admin'),
+    path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('testify/', include('testify.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
---
+++ src/testify/models.py
@@ -0,0 +1,52 @@
+from accounts.models import User
+
+from django.db import models
+
+
+class Test(models.Model):
+    test = models.CharField(max_length=128)
+    user = models.ManyToManyField(
+        to=User,
+        null=True,
+        related_name='tests',
+    )
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=1024)
+    test = models.ForeignKey(
+        to=Test,
+        null=False,
+        on_delete=models.CASCADE,
+        related_name='questions',
+    )
+
+
+class Answer(models.Model):
+    answer = models.CharField(max_length=1024)
+    is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(
+        to=Question,
+        null=False,
+        on_delete=models.CASCADE,
+        related_name='answers',
+    )
+
+
+class Result(models.Model):
+    correct_answers = models.PositiveSmallIntegerField
+    wrong_answers = models.PositiveSmallIntegerField
+    percent = models.PositiveSmallIntegerField
+    user = models.ForeignKey(
+        to=User,
+        null=False,
+        on_delete=models.CASCADE,
+        related_name='results',
+    )
+
+    test = models.ForeignKey(
+        to=Test,
+        null=False,
+        on_delete=models.CASCADE,
+        related_name='results',
+    )
